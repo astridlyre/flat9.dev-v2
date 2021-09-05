4e09a1284184d3db54f569739707d014a57438a4
Erin Burton <eb@flat9.dev>
2021-09-05 03:31:45 -0700

    Sun September 05 2021 @ 03:31:45

--- build.sh
+++ build.sh
@@ -7,7 +7,18 @@ npm run build-rollup
 rm -rf ${DEST}
 
 mkdir -p "${DEST}/assets"
-for item in "./assets/" "./work" "./about" "./contact" "./index.html" "site.webmanifest" "site.jpg" android* apple* fav*; do
+for page in "contact" "work" "about"; do
+	mkdir "${DEST}/${page}"
+done
+
+HEAD="includes/head.html"
+FOOT="includes/footer.html"
+
+for file in "index.html" "work/index.html" "about/index.html" "contact/index.html"; do
+	cat "${HEAD}" "${file}" "${FOOT}" >"${DEST}/${file}"
+done
+
+for item in "./assets/" "site.webmanifest" "site.jpg" android* apple* fav*; do
 	cp -r "${item}" ${DEST}
 done
 
