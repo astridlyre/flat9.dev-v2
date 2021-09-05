d1ee9fe812c5e515d168e52e30d50067a4cde7ae
Erin Burton <eb@flat9.dev>
2021-09-05 02:46:55 -0700

    Sun September 05 2021 @ 02:46:55

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ npm run build-rollup
 rm -rf ${DEST}
 
 mkdir -p "${DEST}/assets"
-for item in "./assets/" "./work" "./about" "./contact" "./index.html" "site.webmanifest" android* apple* fav*; do
+for item in "./assets/" "./work" "./about" "./contact" "./index.html" "site.webmanifest" "site.jpg" android* apple* fav*; do
 	cp -r "${item}" ${DEST}
 done
 
